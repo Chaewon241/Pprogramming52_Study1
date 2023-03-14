e84351af395f3752ff2f9135269ed75e5406713d
2stone0 <127703087+2stone0@users.noreply.github.com>
2023-03-14 12:21:57 +0900

    Create 14681.c

---
+++ baekjoon/조건문/이석영/14681.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+
+int main()
+{
+	int x, y;
+
+	scanf("%d %d", &x, &y);
+
+	if (x < 0)
+	{
+		if (y < 0)
+		{
+			printf("3");
+		}
+		else
+		{
+			printf("2");
+		}
+	}
+	else if (x > 0)
+	{
+		if (y > 0)
+		{
+			printf("1");
+		}
+		else
+		{
+			printf("4");
+		}
+
+		
+	}
+
+	return 0;
+}
