125a55d2231fe46cfd430f4f9dad6cbbb43dd334
2stone0 <127703087+2stone0@users.noreply.github.com>
2023-03-14 12:07:37 +0900

    Create 1330.c

---
+++ baekjoon/조건문/이석영/1330.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+
+int main()
+{
+	int a, b;
+
+	scanf("%d %d", &a, &b);
+	if (a < b)
+	{
+		printf("<");
+	}
+
+	if (a > b)
+	{
+		printf(">");
+	}
+
+	if (a == b)
+	{
+		printf("==");
+	}
+
+	return 0;
+}
