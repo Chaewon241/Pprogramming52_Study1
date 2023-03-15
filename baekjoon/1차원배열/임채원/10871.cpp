edc3d62abe62cb20cc80390df78aaa957a9d8d66
Chaewon <77910498+Chaewon241@users.noreply.github.com>
2023-03-15 10:06:46 +0900

    [임채원] 10871 완료

---
+++ baekjoon/1차원배열/임채원/10871.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n1, n2;
+    cin >> n1 >> n2;
+
+    int arr[100] = { 0 };
+    int idx = 0;
+
+    for (int i = 0; i < n1; i++)
+    {
+        int a;
+        cin >> a;
+
+        
+        if (a < n2)
+            arr[idx++] = a;
+    }
+
+    for (int i = 0; i < idx; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    
+}
